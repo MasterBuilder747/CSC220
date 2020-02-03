74971c893d135623931029f128a35056fa8eb736
Joseph <jaathunder@yahoo.com>
2020-02-03 10:23:11 -0800

    fixed ExpandableArray

--- src/Week02/exampleUsage.java
+++ src/Week02/exampleUsage.java
@@ -13,7 +13,23 @@ public class exampleUsage {
     //example usage
     public static void main(String[] args) throws IOException {
 
-        double[] num1 = readFile("/Users/Joseph/Desktop/numbers.txt");
-        System.out.println(Arrays.toString(num1));
+        //double[] num1 = readFile("/Users/Joseph/Desktop/numbers.txt");
+        //System.out.println(Arrays.toString(num1));
+
+        //test the time that it takes to process larger loops in this function
+
+        //this is quadratic growth
+        for(int size = 10; size <= 1000000; size *= 10) {
+            long start = System.currentTimeMillis();
+            double[] nums = new double[0];
+            for (int i = 0; i < size; i++) {
+                nums = ExpandableArray.expandArray(nums);
+                nums[i] = i;
+            }
+            long end = System.currentTimeMillis();
+            long timeTaken = end - start;
+            System.out.println(size + " : " + timeTaken);
+        }
+
     }
 }
