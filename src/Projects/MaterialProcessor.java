ea4986b97319751a40e6372278a2a4e49e31ba94
Joseph <jaathunder@yahoo.com>
2020-02-04 21:55:44 -0800

    text processing for MaterialProcessor

--- src/Projects/MaterialProcessor.java
+++ src/Projects/MaterialProcessor.java
@@ -1,7 +1,5 @@
 package Projects;
 
-import Lecture.ArrayFuncs.ExpandableArray;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -28,11 +26,11 @@ public class MaterialProcessor {
         fr.close();
     }
 
-    public static String conv(boolean b) {
+    public static String dis(boolean b, String output) {
         if (b) {
-            return "x";
+            return "";
         }else{
-            return " ";
+            return output + ", ";
         }
     }
 
@@ -42,34 +40,110 @@ public class MaterialProcessor {
 
     public static void main(String[] args) throws IOException {
 
-        FileReader fr = new FileReader("/Users/Joseph/Desktop/ingots.txt"); //mac does not need to define the disk, uses / instead of \
+        ///Users/Joseph/Desktop/ingots.txt
+        FileReader fr = new FileReader("C:\\Users\\jaath\\Desktop\\all.txt"); //mac does not need to define the disk, uses / instead of \
         BufferedReader br = new BufferedReader(fr);
-        System.out.println("material\tplate\tdense plate");
-
-        String s = br.readLine();
-        //generate list of materials in separate vars
-        boolean plate, dense_plate, rod, long_rod, gear, small_gear, screw_bolt, rotor, foil, ring, frame, lens, spring, fine_wire = false;
-        String mat = s.substring(0, s.indexOf(" ="));
-        //System.out.println("material: " + mat);
-
-        //read the line and set vars to be able to output
-        plate = test(s, "GENERATE_PLATE");
-        dense_plate = test(s, "GENERATE_DENSE");
-        rod = test(s, "GENERATE_ROD");
-        rod = test(s, "GENERATE_ROD");
-
-        System.out.println(mat + ": \t" + conv(plate));
-/*
         while (true) {
+
+            //read each line of the file
             String s = br.readLine();
-
-            System.out.println(s);
-
             if (s == null) {
                 break;
             }
+
+            //generate list of materials in separate vars
+            boolean plate = false;
+            boolean dense_plate = false;
+            boolean rod = false;
+            boolean long_rod = false;
+            boolean gear = false;
+            boolean small_gear = false;
+            boolean screw_bolt = false;
+            boolean rotor = false;
+            boolean foil = false;
+            boolean ring = false;
+            boolean frame = false;
+            boolean lens = false;
+            boolean spring = false;
+            boolean fine_wire = false;
+
+            //store the value of the material
+            String mat = s.substring(0, s.indexOf(" ="));
+
+            //check to see what the material has generated
+            plate = test(s, "GENERATE_PLATE");
+            dense_plate = test(s, "GENERATE_DENSE");
+            rod = test(s, "GENERATE_ROD");
+            long_rod = test(s, "GENERATE_LONG_ROD");
+            frame = test(s, "GENERATE_FRAME");
+            spring = test(s, "GENERATE_SPRING");
+            gear = test(s, "GENERATE_GEAR");
+            small_gear = test(s, "GENERATE_SMALL_GEAR");
+            rotor = test(s, "GENERATE_ROTOR");
+            ring = test(s, "GENERATE_RING");
+            screw_bolt = test(s, "GENERATE_BOLT_SCREW");
+            foil = test(s, "GENERATE_FOIL");
+            fine_wire = test(s, "GENERATE_FINE_WIRE");
+
+            //broader material values
+            //STD_SOLID = GENERATE_PLATE | GENERATE_ROD | GENERATE_BOLT_SCREW | GENERATE_LONG_ROD;
+            //STD_GEM = GENERATE_ORE | STD_SOLID | GENERATE_LENSE;
+            //STD_METAL = GENERATE_PLATE;
+            //EXT_METAL = STD_METAL | GENERATE_ROD | GENERATE_BOLT_SCREW | GENERATE_LONG_ROD;
+            //EXT2_METAL = EXT_METAL | GENERATE_GEAR | GENERATE_FOIL | GENERATE_FINE_WIRE;
+            if (s.contains("STD_SOLID")) {
+                plate = true;
+                rod = true;
+                screw_bolt = true;
+                long_rod = true;
+            }
+            if (s.contains("STD_GEM")) {
+                plate = true;
+                rod = true;
+                screw_bolt = true;
+                long_rod = true;
+                lens = true;
+            }
+            if (s.contains("STD_METAL")) {
+                plate = true;
+            }
+            if (s.contains("EXT_METAL")) {
+                plate = true;
+                rod = true;
+                screw_bolt = true;
+                long_rod = true;
+            }
+            if (s.contains("EXT2_METAL")) {
+                plate = true;
+                rod = true;
+                screw_bolt = true;
+                long_rod = true;
+                gear = true;
+                fine_wire = true;
+                foil = true;
+            }
+
+            //display the line
+            System.out.println(mat.toLowerCase() + ",");
+            /*
+            System.out.println(mat + ": "
+                    + dis(plate, "plate")
+                    + dis(dense_plate, "dense_plate")
+                    + dis(rod, "rod")
+                    + dis(long_rod, "long rod")
+                    + dis(frame, "frame")
+                    + dis(spring, "spring")
+                    + dis(gear, "gear")
+                    + dis(small_gear, "small_gear")
+                    + dis(rotor, "rotor")
+                    + dis(ring, "ring")
+                    + dis(screw_bolt, "screw_bolt")
+                    + dis(foil, "foil")
+                    + dis(fine_wire, "fine_wire")
+            );
+
+             */
         }
-*/
         fr.close();
     }
 }
