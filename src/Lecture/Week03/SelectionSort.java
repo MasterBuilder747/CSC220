747b1eabfa8c736072b0ce8f52beb27853449139
Joseph <jaathunder@yahoo.com>
2020-02-10 09:38:35 -0800

    add minswap function

--- src/Lecture/Week03/SelectionSort.java
+++ src/Lecture/Week03/SelectionSort.java
@@ -1,5 +1,7 @@
 package Lecture.Week03;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
     public static void swap (double[] a, int i, int j) {
@@ -8,13 +10,32 @@ public class SelectionSort {
         a[i] = x;
     }
 
+    public static int findMinPos(double[] a, int startPos) {
+        //find the minimum position in an array
+        int minPos = startPos;
+        for(int i = startPos + 1; i < a.length; i++) {
+            if (a[i] < a[minPos]) {
+                minPos = i;
+            }
+        }
+        return minPos;
+    }
+
     public static void selectionSort (double[] a) {
         int pos = 0;
-        while(pos < a.length) {
-            int minPos = /* find pos of the smallest element in starting at pos
-            swap a(minPos) and a(pos)*/
+        while(pos < a.length - 1) {
+            int minPos = findMinPos(a, pos); //find pos of the smallest element in starting at pos swap a(minPos) and a(pos)*/
+            swap(a, minPos, pos);
             pos++;
         }
+
+    }
+
+    public static void main(String[] args) {
+        double[] a = {6, 2, 2, 5, 4};
+        System.out.println(Arrays.toString(a));
+        selectionSort(a);
+        System.out.println(Arrays.toString(a));
 
     }
 
