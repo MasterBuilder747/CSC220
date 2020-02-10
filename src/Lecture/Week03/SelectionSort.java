cbbc13c254c3b5cbc9bda6623b2947a2dfb9011c
Joseph <jaathunder@yahoo.com>
2020-02-10 09:55:16 -0800

    fix swap bug

--- src/Lecture/Week03/SelectionSort.java
+++ src/Lecture/Week03/SelectionSort.java
@@ -1,12 +1,19 @@
 package Lecture.Week03;
 
+import Homework.HW1ArrayReview.HW1ArrayReview;
+
 import java.util.Arrays;
 
 public class SelectionSort {
 
+    public static int swapCount;
+    public static int compCount;
+
+    //unfortunately this costs the same amount of work regardless of the array (sorted or not)
     public static void swap (double[] a, int i, int j) {
+        swapCount++;
         double x = a[j];
-        a[i] = a[j];
+        a[j] = a[i];
         a[i] = x;
     }
 
@@ -14,6 +21,7 @@ public class SelectionSort {
         //find the minimum position in an array
         int minPos = startPos;
         for(int i = startPos + 1; i < a.length; i++) {
+            compCount++;
             if (a[i] < a[minPos]) {
                 minPos = i;
             }
@@ -28,14 +36,21 @@ public class SelectionSort {
             swap(a, minPos, pos);
             pos++;
         }
+    }
 
+    public static double[] popA(double[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Math.random();
+        }
+        return a;
     }
 
     public static void main(String[] args) {
-        double[] a = {6, 2, 2, 5, 4};
-        System.out.println(Arrays.toString(a));
+        double[] a = new double[100_000];
+        popA(a);
+        //System.out.println(Arrays.toString(a));
         selectionSort(a);
-        System.out.println(Arrays.toString(a));
+        //System.out.println(Arrays.toString(a));
 
     }
 
