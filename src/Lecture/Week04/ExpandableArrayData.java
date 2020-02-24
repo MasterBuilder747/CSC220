5a164393436bdef78f130cfe1b33233d04af69a6
Joseph <jaathunder@yahoo.com>
2020-02-24 09:27:00 -0800

    add MyArrayList

--- src/Lecture/Week04/ExpandableArrayData.java
+++ src/Lecture/Week04/ExpandableArrayData.java
@@ -1,56 +1,58 @@
 package Lecture.Week04;
 
 public class ExpandableArrayData {
-
-    //one per object (non-static)
-    //static is one variable, global
     //each expandable array object has two attributes:
     //an array and an insert position
 
-        //  each expandable array data object has two attributes, an arry and an insertPos
-        private double[] array;
-        private int insertPos;
+    //cannot be changed, global to all variables
+    private static final int DEFAULT_ARRAY_CAPACITY = 1;
 
-        public ExpandableArrayData() {
-            this.array = new double[1];
-            this.insertPos = 0;
-        }
+    //  each expandable array data object has two attributes, an array and an insertPos
+    // one for each object
+    // add static for one for all objects
+    private double[] array;
+    private int insertPos;
 
-        public ExpandableArrayData(int initialCapacity) {
-            this.array = new double[initialCapacity];
-            this.insertPos = 0;
-        }
+    public ExpandableArrayData() {
+        this.array = new double[1];
+        this.insertPos = 0;
+    }
 
-        //  add the value to the "end" of the expandable array given the pair (array, insertPos)
-        private static void expandIfNecessary(ExpandableArrayData d) {
+    public ExpandableArrayData(int initialCapacity) {
+        this.array = new double[initialCapacity];
+        this.insertPos = 0;
+    }
 
-            //  if the array is full, expand it
-            if (d.array.length == d.insertPos) {
-                //  make a new array K bigger than the old array
-                double[] newArray = new double[d.array.length * 2];
-                //  copy the old array into the new array
-                for (int i = 0; i < d.array.length; i++) {
-                    //  copy one element from the old array into the new array
-                    newArray[i] = d.array[i];
-                }
+    //  add the value to the "end" of the expandable array given the pair (array, insertPos)
+    private static void expandIfNecessary(ExpandableArrayData d) {
 
-                //  set d's array to be the new array
-                d.array = newArray;
+        //  if the array is full, expand it
+        if (d.array.length == d.insertPos) {
+            //  make a new array K bigger than the old array
+            double[] newArray = new double[d.array.length * 2];
+            //  copy the old array into the new array
+            for (int i = 0; i < d.array.length; i++) {
+                //  copy one element from the old array into the new array
+                newArray[i] = d.array[i];
             }
-        }
 
-        //  add the value to the "end" of the expandable array ad
-        public static void add(ExpandableArrayData d, double value) {
-            expandIfNecessary(d);
-            d.array[d.insertPos] = value;
-            d.insertPos++;
+            //  set d's array to be the new array
+            d.array = newArray;
         }
+    }
 
-        public static double get(ExpandableArrayData d, int i) {
-            return d.array[i];
-        }
+    //  add the value to the "end" of the expandable array ad
+    public static void add(ExpandableArrayData d, double value) {
+        expandIfNecessary(d);
+        d.array[d.insertPos] = value;
+        d.insertPos++;
+    }
 
-        public static int getLength(ExpandableArrayData d) {
-            return d.insertPos;
-        }
+    public static double get(ExpandableArrayData d, int i) {
+        return d.array[i];
+    }
+
+    public static int getLength(ExpandableArrayData d) {
+        return d.insertPos;
+    }
 }
---
+++ src/Lecture/Week06/MyArrayList.java
@@ -0,0 +1,14 @@
+package Lecture.Week06;
+
+public class MyArrayList {
+
+    //cannot be changed, global to all variables
+    private static final int DEFAULT_ARRAY_CAPACITY = 1;
+
+    // each myarraylist object has two attributes: an array and an insertPos
+    private double[] array;
+    private int insertPos;
+
+
+
+}
