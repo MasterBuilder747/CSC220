b2c53d1db0e46a0055beab3ef63fd2d2b4d4c797
Joseph <jaathunder@yahoo.com>
2020-03-01 22:33:02 -0800

    finalize hw4

--- src/Homework/HW4/ExpandableArrayData.java
+++ src/Homework/HW4/ExpandableArrayData.java
@@ -6,6 +6,8 @@ Professor: Graham
 
 package Homework.HW4;
 
+import java.lang.StringBuilder;
+
 public class ExpandableArrayData {
 
     //  each expandable array data object has two attributes, an arry and an insertPos
@@ -47,15 +49,81 @@ public class ExpandableArrayData {
         d.insertPos++;
     }
 
-    public static void replace(ExpandableArrayData d, double v, int a) {
-        d.array[a] = v;
-    }
-
     public static double get(ExpandableArrayData d, int i) {
         return d.array[i];
     }
 
     public static int getLength(ExpandableArrayData d) {
         return d.insertPos;
+    }
+
+    //added methods
+    public static void replace(ExpandableArrayData d, double v, int a) {
+        d.array[a] = v;
+    }
+
+    public String toString(ExpandableArrayData d) {
+        //returns a string representation of d
+        // (so a string starting with “[“, ending with “]”,
+        // and showing the elements of d separated by commas).
+        StringBuilder s = new StringBuilder();
+        int length = ExpandableArrayData.getLength(d);
+        for (int i = 0; i < length; i++) {
+            s.append(ExpandableArrayData.get(d, i));
+            if (i != length - 1) {
+                s.append(", ");
+            }
+        }
+        return "[" + s + "]";
+    }
+
+    //this doubles as an addFront and an insert function!
+    public void insert(ExpandableArrayData d, double v, int j) {
+        //– add a new number, v, to position i in d,
+        // moving the numbers above position i up one position.
+        int oldLength = ExpandableArrayData.getLength(d); //this is the old length before expanding
+
+        //move all vars in expandable array data over by 1, inserting into variable v at the front
+        if (j > -1 && j < oldLength + 1) {
+            // store, then replace (so that no extra memory is used)
+            // only one variable will temporarily store the previous var to replace
+            double x;
+
+            //[1, 2, 3, 4, 5]
+            d.insertPos++; //[1, 2, 3, 4, 5, 0]
+            int length = ExpandableArrayData.getLength(d);
+            for (int i = length - 1; i > j; i--) {
+                x = ExpandableArrayData.get(d, i - 1); //x = 5
+                ExpandableArrayData.replace(d, x, i);
+            }
+            ExpandableArrayData.replace(d, v, j);
+        } else {
+            throw new IllegalArgumentException("j cannot be lower than 0 or greater than the array's new length + 1");
+        }
+    }
+
+    public void addFront(ExpandableArrayData d, double v) {
+        //– add a new number, v, to the front of d.
+        insert(d, v, 0);
+    }
+
+    public void delete(ExpandableArrayData d, int j) {
+        //- deletes the entry in position i of d,
+        // moving the numbers above position i down one position.
+
+        int length = ExpandableArrayData.getLength(d);
+
+        if (j > -1 && j < length) {
+
+            //replace after that is deleted
+            for (int i = j; i < length; i++) {
+                ExpandableArrayData.replace(d, ExpandableArrayData.get(d, i + 1), i);
+            }
+            // replace the last element with 0 if that is defined
+            ExpandableArrayData.replace(d, 0, ExpandableArrayData.getLength(d) - 1);
+            d.insertPos--;
+        } else {
+            throw new IllegalArgumentException("j cannot be lower than 0 or greater than the array's new length + 1");
+        }
     }
 }
--- src/Homework/HW4/HW4Main.java
+++ src/Homework/HW4/HW4Main.java
@@ -6,83 +6,65 @@ Professor: Graham
 
 package Homework.HW4;
 
-import Lecture.ArrayFuncs.ExpandableArray;
-
-import java.lang.StringBuilder;
-
 public class HW4Main {
 
-    public static void delete(ExpandableArrayData d, int i) {
-        //- deletes the entry in position i of d,
-        // moving the numbers above position i down one position.
-
-    }
-
-    public static void addFront(ExpandableArrayData d, double v) {
-        //– add a new number, v, to the front of d.
-        // add the element normally, then swap the last element with the first element
-        insert(d, v, 0);
-    }
-
-    //this doubles as an addFront and an insert function!
-    public static void insert(ExpandableArrayData d, double v, int j) {
-        //– add a new number, v, to position i in d,
-        // moving the numbers above position i up one position.
-
-        //move all vars in expandable array data over by 1, inserting into variable v at the front
-        if (j > -1) {
-            // store, then replace (so that no extra memory is used)
-            // only two variables will temporarily store the previous 2 vars
-            double x = v;
-            double y;
-            int length = ExpandableArrayData.getLength(d); //this is the old length before expanding
-
-            //[1, 2, 3]
-            ExpandableArrayData.add(d, 0); //[1, 2, 3, v]
-            for (int i = j; i < length; i += 2) {
-                y = ExpandableArrayData.get(d, i); //y = 1
-                ExpandableArrayData.replace(d, x, i); //[v, 2, 3, v]
-                x = ExpandableArrayData.get(d, i+1); //x = 2
-                ExpandableArrayData.replace(d, y, i+1); //[v, 1, 3, v]...
-            }
-        } else {
-            throw new IllegalArgumentException("j cannot be lower than 0");
-        }
-    }
-
-    public static String toString(ExpandableArrayData d) {
-        //returns a string representation of d
-        // (so a string starting with “[“, ending with “]”,
-        // and showing the elements of d separated by commas).
-        StringBuilder s = new StringBuilder();
-        int length = ExpandableArrayData.getLength(d);
-        for (int i = 0; i < length; i++) {
-            s.append(ExpandableArrayData.get(d, i));
-            if (i != length - 1) {
-                s.append(", ");
-            }
-        }
-        return "[" + s + "]";
-    }
-
     public static void main(String[] args) {
-        //test toString()
+
+        System.out.println("test toString(): ");
+        System.out.println("print out an initialized empty array: ");
         ExpandableArrayData a = new ExpandableArrayData();
-        assert toString(a).equals("[]");
+        assert a.toString(a).equals("[]");
+        System.out.println(a.toString(a));
 
-        //test addFront()
-        for (int i = 1; i < 100; i++) {
-            ExpandableArrayData.add(a, Math.random());
+        System.out.println("print out an array: ");
+        ExpandableArrayData.add(a, 1);
+        assert a.toString(a).equals("[1.0]");
+        System.out.println(a.toString(a));
+
+
+        System.out.println();
+        System.out.println("test addFront(): ");
+        System.out.println("add 100 at the front of the array: ");
+        System.out.println("assert that array contains \"[100.0\":");
+        ExpandableArrayData b = new ExpandableArrayData();
+        for (int i = 0; i < 5; i++) {
+            ExpandableArrayData.add(b, Math.random());
         }
-        addFront(a, 0);
-        assert toString(a).contains("[0.0");
+        System.out.println("input: " + b.toString(b));
+        b.addFront(b, 100);
+        assert b.toString(b).contains("[100.0");
+        assert ExpandableArrayData.getLength(b) == 6;
+        System.out.println("output: " + b.toString(b));
 
-        //test insert()
-        //it is impossible to get 1.0 from Math.random
-        insert(a, 1.0, (int)(Math.random() * 10)); //place in a random position
-        assert toString(a).contains("1.0, ");
 
+        System.out.println();
+        System.out.println("test insert(): ");
+        System.out.println("assert that array contains \"1000.0\":");
+        ExpandableArrayData c = new ExpandableArrayData();
+        for (int i = 0; i < 10; i++) {
+            ExpandableArrayData.add(c, Math.random());
+        }
+        System.out.println("input: " + c.toString(c));
+        c.insert(c, 1000.0, (int)(Math.random() * 10)); //insert at a random position
+        assert c.toString(c).contains("1000.0, ");
+        assert ExpandableArrayData.getLength(c) == 11;
+        System.out.println("output: " + c.toString(c));
+
+
+        System.out.println();
+        System.out.println("test delete(): ");
+        System.out.println("assert that array does not contain \"1.0\":");
+        ExpandableArrayData d = new ExpandableArrayData();
+        for (int i = 0; i < 7; i++) {
+            ExpandableArrayData.add(d, i);
+        }
+        System.out.println("input: " + d.toString(d));
+        d.delete(d, 1);
+        assert ExpandableArrayData.getLength(d) == 6;
+        System.out.println("output: " + d.toString(d));
+        assert !(d.toString(d).contains("1.0"));
+        System.out.println("test an invalid input: ");
+        d.delete(d, -1);
 
     }
-
 }
