8daf94da88bf35493c9bf318b02b3202b9c70303
Joseph <jaathunder@yahoo.com>
2020-04-07 21:18:43 -0700

    add main class

---
+++ src/Homework/HW7/HW7Main.java
@@ -0,0 +1,21 @@
+/*
+Name: Joseph Audras
+Professor: Graham
+Date due: 4-8-20
+Class: CSC 220-1
+*/
+
+package Homework.HW7;
+
+public class HW7Main {
+
+    public static void main(String[] args) {
+        //assert at least twice per method
+
+
+        assert true;
+
+
+    }
+
+}
