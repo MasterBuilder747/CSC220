4ab93ca6e6a6c8758d9b4280a012d79d76a4a4d9
Joseph <jaathunder@yahoo.com>
2020-04-07 19:58:44 -0700

    add hw7

---
+++ src/Homework/HW7/MyLinkedList.java
@@ -0,0 +1,20 @@
+package Homework.HW7;
+
+public class MyLinkedList {
+
+    public void delete(int i) {
+        //– deletes the i-th entry of the list.
+    }
+    public void addMiddle(double element, int i) {
+        //– add a new element to the i-th position of the list.
+    }
+    public void set(double newElement, int i) {
+        //– changes the i-th position of the list to the new element.
+    }
+    public String toString() {
+        //– returns a string representation of the list (so a string starting with “[“,
+        // ending with “]”, and showing the elements of the list separated by commas
+        // – be careful with the last element).
+        return "";
+    }
+}
