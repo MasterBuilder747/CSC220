3a67abe2fdef618736dd82127cf68fd49220ce83
Joseph <jaathunder@yahoo.com>
2020-04-16 12:03:41 -0700

    add hw8 main and example code

---
+++ src/Homework/HW8/Main.java
@@ -0,0 +1,43 @@
+/*
+Name: Joseph Audras
+Professor: Graham
+Date due: 4-17-20
+Class: CSC 220-1
+*/
+
+package Homework.HW8;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+/*
+    // create a list of 4 strings
+    ArrayList<String> L = new ArrayList<>();
+L.add("Facebook");
+L.add("Paypal");
+L.add("Google");
+L.add("Yahoo");
+// loop/iterate over the list using a simple for loop
+System.out.println("Simple for loop");
+for (int i = 0; i < L.size(); i++)
+    {
+        String x = L.get(i);
+        System.out.println(x);
+    }
+// loop/iterate over the list using a foreach loop
+System.out.println("Foreach loop");
+for (String x : L)
+    {
+        System.out.println(x);
+    }
+// loop/iterate over the list using an iterator
+System.out.println("Iterator based loop");
+    Iterator<String> it = L.iterator();
+while (it.hasNext())
+    {
+        String x = it.next();
+        System.out.println(x);
+    }
+*/
+}
