a2971b2eac2a467b649f1fdaad333425503e34ec
Joseph <jaathunder@yahoo.com>
2020-04-19 20:21:28 -0700

    Q1 and Q2 done

--- src/Homework/HW8/Main.java
+++ src/Homework/HW8/Main.java
@@ -9,9 +9,11 @@ package Homework.HW8;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main {
 
+    //Q1: ArrayList
     public static int addFor(ArrayList<Integer> al) {
         int sum = 0;
         for (int i = 0; i < al.size(); i++) {
@@ -19,23 +21,73 @@ public class Main {
         }
         return sum;
     }
-
     public static int addForeach(ArrayList<Integer> al) {
         int sum = 0;
+        for (int i : al) {
+            sum += al.get(i);
+        }
+        return sum;
+    }
+    public static int addIt(ArrayList<Integer> al) {
+        int sum = 0;
+        for (Iterator<Integer> itr=al.iterator(); itr.hasNext();) {
+            sum += itr.next();
+        }
         return sum;
     }
 
-    public static int addIt(ArrayList<Integer> al) {
+    //Q2: LinkedList
+    public static int addFor(LinkedList<Integer> al) {
         int sum = 0;
+        for (int i = 0; i < al.size(); i++) {
+            sum += al.get(i);
+        }
+        return sum;
+    }
+    public static int addForeach(LinkedList<Integer> al) {
+        int sum = 0;
+        for (int i : al) {
+            sum += al.get(i);
+        }
+        return sum;
+    }
+    public static int addIt(LinkedList<Integer> al) {
+        int sum = 0;
+        for (Iterator<Integer> itr=al.iterator(); itr.hasNext();) {
+            sum += itr.next();
+        }
         return sum;
     }
 
     public static void main(String[] args) {
+        //Q1: ArrayList
         ArrayList<Integer> al = new ArrayList<>();
+        ArrayList<Integer> al2 = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             al.add(i);
         }
-        System.out.println("For loop: " + addFor(al));
+        assert addFor(al) == 45;
+        assert addForeach(al) == 45;
+        assert addIt(al) == 45;
+        assert addIt(al2) == 0;
+
+        //Q2: LinkedList
+        LinkedList<Integer> ll = new LinkedList<>();
+        LinkedList<Integer> ll2 = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            ll.add(i);
+        }
+        assert addFor(ll) == 45;
+        assert addForeach(ll) == 45;
+        assert addIt(ll) == 45;
+        assert addIt(ll2) == 0;
+
+        //Q3: plot Q1
+
+
+        //Q4: plot Q2
+
+
     }
 
 }
