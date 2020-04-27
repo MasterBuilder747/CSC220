ba04cbf67d3933b0207407f99accb4b3291acc42
Joseph <jaathunder@yahoo.com>
2020-04-27 08:09:00 -0700

    finalize hw9 pt2

--- src/Homework/HW9/HW9Main.java
+++ src/Homework/HW9/HW9Main.java
@@ -75,9 +75,9 @@ public class HW9Main {
         assert aveDepth(bt3) == 0;
         assert aveDepth(bt4) == 1;
 
-        int M = 10;
-        int[] a = {2, 4, 7, 10, 12, 15, 100, 1000, 10000};
-        double[][] aves = new double[9][M];
+        int M = 100;
+        int[] a = {2, 4, 7, 10, 12, 15, 30, 50, 100, 500, 1000, 5000, 10000, 50000, 100000};
+        double[][] aves = new double[a.length][M];
 
         for (int i = 0; i < M; i++) {
             BST<Double, Double> b3 = randomD(a[0]);
@@ -89,6 +89,13 @@ public class HW9Main {
             BST<Double, Double> b9 = randomD(a[6]);
             BST<Double, Double> b10 = randomD(a[7]);
             BST<Double, Double> b11 = randomD(a[8]);
+            BST<Double, Double> b12 = randomD(a[9]);
+            BST<Double, Double> b13 = randomD(a[10]);
+            BST<Double, Double> b14 = randomD(a[11]);
+            BST<Double, Double> b15 = randomD(a[12]);
+            BST<Double, Double> b16 = randomD(a[13]);
+            BST<Double, Double> b17 = randomD(a[13]);
+            BST<Double, Double> b18 = randomD(a[14]);
 
             aves[0][i] = aveDepth(b3);
             aves[1][i] = aveDepth(b4);
@@ -99,11 +106,18 @@ public class HW9Main {
             aves[6][i] = aveDepth(b9);
             aves[7][i] = aveDepth(b10);
             aves[8][i] = aveDepth(b11);
+            aves[9][i] = aveDepth(b12);
+            aves[10][i] = aveDepth(b13);
+            aves[11][i] = aveDepth(b14);
+            aves[12][i] = aveDepth(b15);
+            aves[13][i] = aveDepth(b16);
+            aves[13][i] = aveDepth(b17);
+            aves[14][i] = aveDepth(b18);
         }
 
         //print the plot data
         System.out.println("N,average depth");
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < M; j++) {
                 if (j == 0)
                     System.out.print(a[i] + ",");
