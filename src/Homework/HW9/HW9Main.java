925d293ccfaf42b146c47a998019d5c691a7153c
Joseph <jaathunder@yahoo.com>
2020-04-27 07:51:15 -0700

    finalize hw9

--- src/Homework/HW9/HW9Main.java
+++ src/Homework/HW9/HW9Main.java
@@ -20,6 +20,17 @@ public class HW9Main {
         return b;
     }
 
+    public static BST<Double, Double> randomD(int N) {
+        BST<Double, Double> b = new BST<>();
+        if (N > 0) {
+            for (int i = 0; i < N; i++) {
+                //key = 1, value = rand
+                b.put(Math.random(), 1.0);
+            }
+        }
+        return b;
+    }
+
     public static BST<Double, Double> ordered (int N) {
         BST<Double, Double> b = new BST<>();
         if (N > 0) {
@@ -57,16 +68,52 @@ public class HW9Main {
         //and compute the average of their depths.
         BST<Double, Double> bt1 = ordered(0);
         BST<Double, Double> bt2 = ordered(-1);
-        BST<Double, Double> bt3 = ordered(4);
+        BST<Double, Double> bt3 = ordered(1);
+        BST<Double, Double> bt4 = ordered(3);
         assert aveDepth(bt1) == 0;
         assert aveDepth(bt2) == 0;
-        assert aveDepth(bt3) == 1;
+        assert aveDepth(bt3) == 0;
+        assert aveDepth(bt4) == 1;
 
-        BST<Double, Double> b4 = ordered(4);
-        BST<Double, Double> b5 = ordered(7);
-        BST<Double, Double> b6 = ordered(10);
-        BST<Double, Double> b7 = ordered(12);
-        BST<Double, Double> b8 = ordered(15);
+        int M = 10;
+        int[] a = {2, 4, 7, 10, 12, 15, 100, 1000, 10000};
+        double[][] aves = new double[9][M];
+
+        for (int i = 0; i < M; i++) {
+            BST<Double, Double> b3 = randomD(a[0]);
+            BST<Double, Double> b4 = randomD(a[1]);
+            BST<Double, Double> b5 = randomD(a[2]);
+            BST<Double, Double> b6 = randomD(a[3]);
+            BST<Double, Double> b7 = randomD(a[4]);
+            BST<Double, Double> b8 = randomD(a[5]);
+            BST<Double, Double> b9 = randomD(a[6]);
+            BST<Double, Double> b10 = randomD(a[7]);
+            BST<Double, Double> b11 = randomD(a[8]);
+
+            aves[0][i] = aveDepth(b3);
+            aves[1][i] = aveDepth(b4);
+            aves[2][i] = aveDepth(b5);
+            aves[3][i] = aveDepth(b6);
+            aves[4][i] = aveDepth(b7);
+            aves[5][i] = aveDepth(b8);
+            aves[6][i] = aveDepth(b9);
+            aves[7][i] = aveDepth(b10);
+            aves[8][i] = aveDepth(b11);
+        }
+
+        //print the plot data
+        System.out.println("N,average depth");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < M; j++) {
+                if (j == 0)
+                    System.out.print(a[i] + ",");
+                else if (j == M - 1)
+                    System.out.print(aves[i][j]);
+                else
+                    System.out.print(aves[i][j] + ",");
+            }
+            System.out.println();
+        }
     }
 
 }
